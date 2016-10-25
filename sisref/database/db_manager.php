cd74827e1cc102627d55f569054c6c1fd639dbaa
Fernanda von Borries <fernandavonborries@gmail.com>
2016-10-25 11:14:05 -0200

    Atualização

---
+++ sisref/database/db_manager.php
@@ -0,0 +1,22 @@
+﻿<?php
+class DBConnector extends PDO {
+   
+    private $engine;
+    private $host;
+    private $database;
+    private $user;
+    private $pass;
+    private $connector;	
+   
+    public function __construct(){
+        $this->engine = 'mysql';
+        $this->host = 'localhost';
+        $this->database = 'db_sisref';
+        $this->user = 'root';
+        $this->pass = 'root';
+        $dns = $this->engine.':dbname='.$this->database.";host=".$this->host;
+        parent::__construct( $dns, $this->user, $this->pass );
+    }			
+}
+//$result = (new DBConnector())->query('SELECT * FROM tb_user');
+//var_dump($result->fetchAll());
---
+++ sisref/index.php
@@ -0,0 +1,4 @@
+﻿<?php
+include('../sisref/control/request_controller.php');
+$controller = new RequestController();
+echo json_encode($controller->execute());
